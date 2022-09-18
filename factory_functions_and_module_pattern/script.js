a894c4976068121cb9311f3ce3f9b5d5755b1195
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-18 23:27:00 +0600

    Inharitance with factories

--- factory_functions_and_module_pattern/script.js
+++ factory_functions_and_module_pattern/script.js
@@ -5,6 +5,50 @@ const personFactory = (name, age) => {
 };
 const rafee = personFactory("Rafee", 22);
 
-console.log(rafee);
-console.log(rafee.name);
-rafee.sayHello();
+// console.log(rafee);
+// console.log(rafee.name);
+// rafee.sayHello();
+
+//Private variables and function:
+const FactoryFunction = (string) => {
+  const capital = () => string.toUpperCase();
+  const print = () => console.log(capital());
+  return { print };
+};
+
+const rafi = FactoryFunction("rafi");
+// print();
+// capital();
+// rafi.capital();
+rafi.print();
+
+const funCounter = () => {
+  let count = 1;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+
+const counter = funCounter();
+// counter();
+// counter();
+// counter();
+
+// Inheritance with factories:
+const Person = (name) => {
+  const sayName = () => console.log(`my name is ${name}`);
+  return { sayName };
+};
+
+const Nerd = (name) => {
+  const { sayName } = Person(name);
+
+  const doSomethingNerdy = () => console.log("nerd stuff");
+  return { sayName, doSomethingNerdy };
+};
+
+const raf = Nerd("Rafee");
+
+raf.sayName();
+raf.doSomethingNerdy();
