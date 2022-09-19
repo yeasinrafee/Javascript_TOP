bcacdb2a47094dd526020a3c3253e682bfd13f39
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-19 16:35:55 +0600

    Static Method

--- Classes/index.js
+++ Classes/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 //Function Constructor:
-
 // function Person(firstName, lastName) {
 //   let code = 44535;
 
@@ -23,7 +22,37 @@
 
 // console.log(rafee);
 
+// Class Constructor:
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+
+//     this.code = 34563;
+//   }
+
+//   getFirstName(firstName) {
+//     this.firstName = firstName;
+//   }
+//   getLastName(lastName) {
+//     this.lastName = lastName;
+//   }
+//   fullName() {
+//     console.log(`${this.firstName} ${this.lastName}`);
+//   }
+//   getCode() {
+//     console.log(this.code);
+//   }
+// }
+
+// const rafee = new Person("Yeasin", "Rafee");
+
+// Static Method:
 class Person {
+  static species() {
+    console.log("Homo sapiens");
+  }
+
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
